f8d38ca1da15f4574c662f97359d693185d20cad
Coman Luiza <56609144+CoLuiza@users.noreply.github.com>
2020-04-29 18:52:53 +0300

    Add admin filter for model Case (#6)
    * Add admin filter for Case model
    
    * Fix filters

--- backend/api_core/admin.py
+++ backend/api_core/admin.py
@@ -4,8 +4,41 @@ from django.utils.safestring import mark_safe
 from api_core.models import Case, DataSource, Hospital, Comorbidity, Statistic
 
 
+class AgeFilter(admin.SimpleListFilter):
+    title = 'Vârstă'
+
+    parameter_name = 'Vârstă'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('020', '0 - 20'),
+            ('2040', '20 - 40'),
+            ('4060', '40 - 60'),
+            ('6080', '60 - 80'),
+            ('80100', '80 - 100'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '020':
+            return queryset.filter(age__gte=0,
+                                   age__lte=20)
+        if self.value() == '2040':
+            return queryset.filter(age__gte=20,
+                                   age__lte=40)
+        if self.value() == '4060':
+            return queryset.filter(age__gte=40,
+                                   age__lte=60)
+        if self.value() == '6080':
+            return queryset.filter(age__gte=60,
+                                   age__lte=80)
+        if self.value() == '80100':
+            return queryset.filter(age__gte=80,
+                                   age__lte=100)
+
+
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
+    list_filter = ("validated", "gender", AgeFilter, "county")
     readonly_fields = ['source', 'parsed_text']
     ordering = ('-case_number', 'collision_index')
     list_display = ('case_number', 'collision_index', 'gender', 'age', 'county',
