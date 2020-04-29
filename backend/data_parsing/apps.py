208002f21e8eaf12f7298b416e399c37f6fbfc6a
Coman Luiza <56609144+CoLuiza@users.noreply.github.com>
2020-04-29 20:44:38 +0300

    Scheduler implemented (#8)
    * Try to implement scheduler
    
    * Fix scheduler
    
    * Fix scheduler

--- backend/data_parsing/apps.py
+++ backend/data_parsing/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 
+from backend import settings
+
 
 class DataParsingConfig(AppConfig):
     name = 'data_parsing'
+
+    def ready(self):
+        from . import scheduler
+        if settings.SCHEDULER_AUTOSTART:
+            scheduler.start()
---
+++ backend/data_parsing/scheduler.py
@@ -0,0 +1,21 @@
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.conf import settings
+from django_apscheduler.jobstores import register_events
+
+from data_parsing.tasks import process_data_task
+
+scheduler = BackgroundScheduler(settings.SCHEDULER_CONFIG)
+
+
+def start():
+    if settings.DEBUG:
+        logging.basicConfig()
+        logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+    scheduler.add_job(process_data_task, "cron", id="Process data", hour="*/3", minute="0", replace_existing=True)
+    logging.info("Job started successfully!")
+    register_events(scheduler)
+
+    scheduler.start()
